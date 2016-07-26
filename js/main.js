c4b9b01303c93b92e6701e518c30a2f6d9c871b8
necoline <hnhubner@gmail.com>
2016-07-25 18:15:48 -0600

    finishing functionality

--- js/main.js
+++ js/main.js
@@ -3,17 +3,23 @@
 //function - onclick deliver randomized answer
 //functino - deliver computer answers in answer feild and history feild//
 
-var wisdoms = ["Ofcourse!", "That's a terrible thought", "yes", "no", "maybe?", "It is decidedly so", "It doesn't matter, you're awesome", "Just take it slow", "Keep is drama free", "How could you even ask that?", "Ask me later", "Can you rephrase that?", "I wouldn't get your hopes up"];
+var wisdoms = ["Ofcourse!", "That's a terrible thought.", "Indeed.", "Not a chance.", "Maybe?", "It is decidedly so", "It doesn't matter, you're awesome.", "Just take it slow.", "Keep is drama free.", "How could you even ask that?", "Ask me later.", "Can you rephrase that?", "I wouldn't get your hopes up."];
 
 
 function response() {
   var userAnswer = document.getElementById("userAnswer");
   var history = document.getElementById("history");
   var input = document.getElementById("input");
+  var li = document.createElement("li");
   var computerAnswer = wisdoms[Math.floor(Math.random()* wisdoms.length)];
 
   if (input !== "") {
     userAnswer.innerHTML = computerAnswer;
-
+    li.innerHTML = input.value + " \:" + computerAnswer;
+    history.appendChild(li);
+    document.getElementById("input").value = "";
+  }
+  else {
+    alert("Please ask me a question");
   }
 }
